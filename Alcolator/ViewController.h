9fd91be46462d4af630ba53bda27433eff455f35
Fiona Alpe <fiona.alpe@gmail.com>
2016-05-13 21:23:30 +0300

    Assignment 19 not yet complete need to position the label

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Fiona Alpe on 5/12/16.
+//  Copyright © 2016 Fiona Alpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
